304e9bfaa74cc167463768cf8cc719cb006b1d45
Marco Valerio <mo.valerio93@gmal.com>
2022-07-22 10:04:44 +0200

    bonus done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!'
+      message: 'Hello Vue',
+      image: 'https://static.geopop.it/wp-content/uploads/sites/32/2022/07/james-webb-prime-immagini.jpeg',
     }
   })
